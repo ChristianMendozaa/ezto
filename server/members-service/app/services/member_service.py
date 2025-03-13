18edf3477bb071afb19e6b667ac4a22c5bf1690f
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2025-03-13 13:04:58 -0400

    arreglos

--- server/members-service/app/services/member_service.py
+++ server/members-service/app/services/member_service.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app.repositories.member_repository import MemberRepository
 
 class MemberService:
@@ -21,27 +20,3 @@ class MemberService:
     async def delete_member(member_id):
         """Elimina un miembro por su ID."""
         return await MemberRepository.delete_member(member_id)
-=======
-from app.repositories.member_repository import MemberRepository
-
-class MemberService:
-    @staticmethod
-    async def list_members():
-        """Obtiene todos los miembros."""
-        return await MemberRepository.get_all_members()
-
-    @staticmethod
-    async def create_member(member_data):
-        """Crea un nuevo miembro."""
-        return await MemberRepository.create_member(member_data)
-
-    @staticmethod
-    async def update_member(member_id, member_data):
-        """Actualiza un miembro existente."""
-        return await MemberRepository.update_member(member_id, member_data)
-
-    @staticmethod
-    async def delete_member(member_id):
-        """Elimina un miembro por su ID."""
-        return await MemberRepository.delete_member(member_id)
->>>>>>> afb75bf933e10a27a8164a48c8899b5b816ddf92
